45e25822d01769324f4656e7a25b961360b68ed9
Lakshya <agarwallakshya94@gmail.com>
2026-08-09 12:14:38 +0530

    feat: add i18n support with hindi and english locales

---
+++ i18n/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+import hi from './locales/hi.json';
+
+const resources = {
+  en: { translation: en },
+  hi: { translation: hi }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    // @ts-ignore
+    compatibilityJSON: 'v3',
+    resources,
+    lng: 'hi', // default language
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+export default i18n;
